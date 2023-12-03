5bdcd2fd0b33c25fd961925c5194d951d7b45930
staidigital <sondre.stai@gmail.com>
2023-12-03 20:11:59 +0100

    Day 3 part 1 solution

---
+++ 3/3_part1.py
@@ -0,0 +1,34 @@
+import os, math
+path = os.getcwd()
+data = open(path + "/3/data.txt").readlines()
+data = [elem.strip() for elem in data]
+ref_nums = "0123456789"
+parts_sum = 0
+
+def findEntireNumber(text, index):
+    num = ""
+    for i in range(len(text)):
+        if text[i] in ref_nums:
+            num += text[i]
+        else:
+            break
+    return num
+
+for i in range(0, len(data)):
+    j = 0
+    while j < len(data[i])-1:
+        if data[i][j] in ref_nums:
+            entireNumber = findEntireNumber(data[i][j:], j)
+            endRange = j + len(entireNumber) + 1 if j + len(entireNumber) + 1 < len(data[i]) else j + len(entireNumber)
+            startRange = j - 1 if j - 1 > 0 else j
+            for k in range(j-1,endRange):
+                if data[i-1][k] not in ref_nums and data[i-1][k] != "." and i != 0:
+                    parts_sum += int(entireNumber)
+                if data[i][k] not in ref_nums and data[i][k] != ".":
+                    parts_sum += int(entireNumber)
+                if i != len(data)-1 and data[i+1][k] not in ref_nums and data[i+1][k] != ".":
+                    parts_sum += int(entireNumber)
+            j = j + len(entireNumber)
+        else:
+            j += 1
+print(parts_sum)
